bf1cee7faf4a93c5a17b41a00395d97a87a1c37e
dpg3 <dguarin0@chicagobooth.edu>
2021-10-30 12:28:36 +0000

    Step 2 done

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -9,4 +9,17 @@ class CurrenciesController < ApplicationController
 
     render({ :template => "currency_templates/step_one.html.erb"})
   end
+
+  def second_currency
+    @first_symbol = params.fetch("currency").to_s
+
+    @raw_data = open("https://api.exchangerate.host/symbols").read
+    @parsed_data = JSON.parse(@raw_data)
+    @symbols_hash = @parsed_data.fetch("symbols")
+    
+    @array_of_symbols = @symbols_hash.keys
+
+  render({ :template => "currency_templates/step_two.html.erb"})
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   get("/forex", { :controller => "currencies", :action => "first_currency"})
+
+  get("/forex/:currency", { :controller => "currencies", :action => "second_currency"})
 end
